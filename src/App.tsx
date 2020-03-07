878d8237cc16ea629dc99bf442f7eea7c853d07e
Vsevolod Ivanov <fromdaytoday@icloud.com>
2020-03-07 21:22:17 +0300

    sass, container, mixins, workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from "./components/Header/Header";
-import "./App.css";
+import "./styles/styles.sass";
 
 function App() {
 	return (
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
+import s from "./Header.module.sass"
 
 type PropsType =  {
 
 }
 
 const Header = (props: PropsType) => {
-	return <header>header</header>;
+	return (
+		<header className={s.header}>
+			<div className="container">
+				<div className={s.header__inner}>
+					<div className={s.header__logo}>
+						logo
+					</div>
+					<div className={s.header__info}>
+						<div className={s.header__controls}>
+							controls
+						</div>
+						<div className={s.header__social}>
+							<a href="!#">FB</a>
+							<a href="!#">TW</a>
+							<a href="!#">link</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
